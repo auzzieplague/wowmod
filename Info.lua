e46d3ef6864f4d98fc0524cbfd49b5c8b39eb0a0
auzzieplague <auzzieplague@gmail.com>
2024-07-26 22:52:24 +1000

    main

--- Info.lua
+++ Info.lua
@@ -1,32 +1,31 @@
---local output ="INSTANCE_CHAT";
+-- Output configuration
 local output ="SAY";
 
-
---roles and spec tables
-specs={
- "Blood", "Frost","Unholy", "Balance","Feral",
-"Guardian", "Restoration","Beast Mastery","Marksmanship",
- "Survival", "Arcane", "Fire", "Frost", "Brewmaster",
- "Mistweaver", "Windwalker", "Holy", "Protection",
- "Retribution", "Discipline", "Holy", "Shadow",
- "Assassination", "Combat", "Subtlety", "Elemental",
- "Enhancement", "Restoration", "Affliction", "Demonology",
- "Destruction", "Arms", "Fury", "Protection",
- "Vengeance","Havoc"
+-- Roles and spec tables
+specs = {
+    "Blood", "Frost", "Unholy", "Balance", "Feral",
+    "Guardian", "Restoration", "Beast Mastery", "Marksmanship",
+    "Survival", "Arcane", "Fire", "Frost", "Brewmaster",
+    "Mistweaver", "Windwalker", "Holy", "Protection",
+    "Retribution", "Discipline", "Holy", "Shadow",
+    "Assassination", "Combat", "Subtlety", "Elemental",
+    "Enhancement", "Restoration", "Affliction", "Demonology",
+    "Destruction", "Arms", "Fury", "Protection",
+    "Vengeance", "Havoc"
 }
-roles={
- "Tank", "Melee", "Melee", "Ranged", "Melee",
- "Tank", "Healer", "Ranged", "Ranged",
- "Ranged", "Ranged", "Ranged", "Ranged", "Tank",
- "Healer", "Melee", "Healer", "Tank",
- "Melee", "Healer", "Healer", "Ranged",
- "Melee", "Melee", "Melee", "Ranged",
- "Melee", "Healer", "Ranged", "Ranged",
- "Ranged", "Melee", "Melee", "Tank",
- "Tank","Melee"
+roles = {
+    "Tank", "Melee", "Melee", "Ranged", "Melee",
+    "Tank", "Healer", "Ranged", "Ranged",
+    "Ranged", "Ranged", "Ranged", "Ranged", "Tank",
+    "Healer", "Melee", "Healer", "Tank",
+    "Melee", "Healer", "Healer", "Ranged",
+    "Melee", "Melee", "Melee", "Ranged",
+    "Melee", "Healer", "Ranged", "Ranged",
+    "Ranged", "Melee", "Melee", "Tank",
+    "Tank", "Melee"
 }
 
---setup output text over box
+-- Setup output text over box
 INFO_TextFrame = CreateFrame("Frame");
 INFO_TextFrame:ClearAllPoints();
 INFO_TextFrame:SetHeight(300);
@@ -36,31 +35,30 @@ INFO_TextFrame:Hide();
 INFO_TextFrame.text = INFO_TextFrame:CreateFontString(nil, "BACKGROUND", "GameFontNormalSmall");
 INFO_TextFrame.text:SetAllPoints();
 INFO_TextFrame.text:SetJustifyH("LEFT");
-INFO_TextFrame.left=5;
-INFO_TextFrame.top=-170;
+INFO_TextFrame.left = 5;
+INFO_TextFrame.top = -170;
 INFO_TextFrame:SetPoint("LEFT", INFO_TextFrame.left, INFO_TextFrame.top);
 INFO_TextFrameTime = 0;
-INFO_TextFrame.sticky=false;
-INFO_TextFrame.fadetime=5;
+INFO_TextFrame.sticky = false;
+INFO_TextFrame.fadetime = 5;
 
-
--- fade function (must not be local for callbacks)
+-- Fade function
 function INFO_TextFrame_OnUpdate()
-  if (INFO_TextFrame.sticky == false ) then 
-      if (INFO_TextFrameTime < GetTime() - INFO_TextFrame.fadetime) then
-        local alpha = INFO_TextFrame:GetAlpha();
-        if (alpha ~= 0) then INFO_TextFrame:SetAlpha(alpha - .05); end
-        if (alpha == 0) then INFO_TextFrame:Hide(); end
-      end
-   end
+    if (INFO_TextFrame.sticky == false) then
+        if (INFO_TextFrameTime < GetTime() - INFO_TextFrame.fadetime) then
+            local alpha = INFO_TextFrame:GetAlpha();
+            if (alpha ~= 0) then INFO_TextFrame:SetAlpha(alpha - .05); end
+            if (alpha == 0) then INFO_TextFrame:Hide(); end
+        end
+    end
 end
 
--- put text on screen
+-- Put text on screen
 function INFO_TextMessage(message)
-  INFO_TextFrame.text:SetText(message);
-  INFO_TextFrame:SetAlpha(1);
-  INFO_TextFrame:Show();
-  INFO_TextFrameTime = GetTime();
+    INFO_TextFrame.text:SetText(message);
+    INFO_TextFrame:SetAlpha(1);
+    INFO_TextFrame:Show();
+    INFO_TextFrameTime = GetTime();
 end
 
 local function ShowCommandList()
@@ -69,297 +67,233 @@ local function ShowCommandList()
     cmds = cmds.."bg, ashran, healers, tanks\n";
     cmds = cmds.."cleanbags, emptybags, vendorall\n";
     cmds = cmds.."cleantracker";
-    print (cmds);
+    print(cmds);
 end
 
---item max refers to cuttof point 4=rare
+-- Item max refers to cutoff point 4=rare
 local function VendorBags(itemMax)
-    for b=0,4 
-    do for s=1,GetContainerNumSlots(b) 
-        do l=GetContainerItemLink(b,s) 
-            if l then 
-                itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice=GetItemInfo(l) 
-                if (itemRarity < itemMax) then 
-                    if (itemName ~= "Hearthstone") then 
-                        UseContainerItem(b,s)
+    for b = 0, 4 do
+        for s = 1, C_Container.GetContainerNumSlots(b) do
+            local l = C_Container.GetContainerItemLink(b, s)
+            if l then
+                local itemName, itemLink, itemRarity = GetItemInfo(l)
+                if (itemRarity < itemMax) then
+                    if (itemName ~= "Hearthstone") then
+                        C_Container.UseContainerItem(b, s)
                     end
-                    
-                end 
-            end 
-        end 
+                end
+            end
+        end
     end
 end
 
---item max refers to cuttof point 4=rare
-local function CleanBags (itemMax)
-    for b=0,4 
-    do for s=1,GetContainerNumSlots(b) 
-        do l=GetContainerItemLink(b,s) 
-            if l then 
-                itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice=GetItemInfo(l) 
-                if (itemRarity < itemMax) then 
-                    if (itemName ~= "Hearthstone") then 
-                        PickupContainerItem(b,s) DeleteCursorItem()
-                        --print (itemLink.." > "..itemRarity)
+-- Item max refers to cutoff point 4=rare
+local function CleanBags(itemMax)
+    for b = 0, 4 do
+        for s = 1, C_Container.GetContainerNumSlots(b) do
+            local l = C_Container.GetContainerItemLink(b, s)
+            if l then
+                local itemName, itemLink, itemRarity = GetItemInfo(l)
+                if (itemRarity < itemMax) then
+                    if (itemName ~= "Hearthstone") then
+                        C_Container.PickupContainerItem(b, s)
+                        DeleteCursorItem()
                     end
-                    
-                end 
-            end 
-        end 
+                end
+            end
+        end
     end
 end
 
-
--- cleanup objective / achievement tracker
-local function CleanTracker ()
-    local x=GetNumQuestWatches();
-    for i=0,x-1 do 
-        RemoveQuestWatch(GetQuestIndexForWatch(x-i));
-    end 
-    local tracked={GetTrackedAchievements()} 
-    for i=1,#tracked do 
+-- Cleanup objective / achievement tracker
+local function CleanTracker()
+    local x = GetNumQuestWatches();
+    for i = 0, x - 1 do
+        RemoveQuestWatch(GetQuestIndexForWatch(x - i));
+    end
+    local tracked = {GetTrackedAchievements()}
+    for i = 1, #tracked do
         RemoveTrackedAchievement(tracked[i]);
     end
 end
 
--- initial call from xml file
+-- Initial call from XML file
 function Info(self)
     ShowCommandList();
 end
 
-
--- slash command handler
-local function handler(msg, editbox)    
-     if msg == 'me' then
-      local txt= "server:"..GetRealmName().."\n";
-        txt=txt.."title:"..GetCurrentTitle().."\n";
-        txt=txt.."stats:";
+-- Slash command handler
+local function handler(msg, editbox)
+    if msg == 'me' then
+        local txt = "server:" .. GetRealmName() .. "\n";
+        txt = txt .. "title:" .. GetCurrentTitle() .. "\n";
+        txt = txt .. "stats:";
         INFO_TextMessage(txt);
-     elseif msg == 'title' then
-      INFO_TextMessage(GetCurrentTitle());
-     elseif msg == 'sticky' then
+    elseif msg == 'title' then
+        INFO_TextMessage(GetCurrentTitle());
+    elseif msg == 'sticky' then
         ToggleSticky();
-     elseif msg =='bg' then
+    elseif msg == 'bg' then
         GetBGInfo();
-	elseif msg =='healers' then
+    elseif msg == 'healers' then
         GetHealers();
-     elseif msg =='stats' then
+    elseif msg == 'stats' then
         GetSpecInfo();
-     elseif msg =='cleantracker' then
+    elseif msg == 'cleantracker' then
         CleanTracker();
-     elseif msg =='cleanbags' then
+    elseif msg == 'cleanbags' then
         CleanBags(4);
-      elseif msg =='emptybags' then
+    elseif msg == 'emptybags' then
         CleanBags(8);
-      elseif msg =='vendorall' then
+    elseif msg == 'vendorall' then
         VendorBags(8);
-     else
+    else
         ShowCommandList();
-     end
+    end
 end
 
---slash command binding
+-- Slash command binding
 SLASH_INFO1, SLASH_INFO2 = '/info', '/get';
-SlashCmdList["INFO"] = handler; -- Also a valid assignment strategy
+SlashCmdList["INFO"] = handler;
 
--- toggle sticky text
-function ToggleSticky ()
-     if (INFO_TextFrame.sticky==false) then
-        INFO_TextFrame.sticky=true;
+-- Toggle sticky text
+function ToggleSticky()
+    if (INFO_TextFrame.sticky == false) then
+        INFO_TextFrame.sticky = true;
         INFO_TextMessage("Sticky:On");
     else
-        INFO_TextFrame.sticky=false;
+        INFO_TextFrame.sticky = false;
         INFO_TextMessage("Sticky:Off");
     end
 end
 
-function GetRoleFromSpec (spec)
-    i=1;
-    while specs[i] do
-        if specs[i]==spec then 
+function GetRoleFromSpec(spec)
+    for i, v in ipairs(specs) do
+        if v == spec then
             return roles[i]
         end
-    i=i+1
     end
     return "error"
 end
 
-function GetRaidLeader ()
-    local leader="unkown";
-    for i=1, GetNumGroupMembers() do
-        name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
-        if (rank==2) then 
-            leader=name;
-        end    
+function GetRaidLeader()
+    local leader = "unknown";
+    for i = 1, GetNumGroupMembers() do
+        local name, rank = GetRaidRosterInfo(i);
+        if (rank == 2) then
+            leader = name;
+        end
     end
     return leader;
 end
 
- --convoluted lua split string method
+-- Convoluted Lua split string method
 function split(str, pat)
-   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-   local fpat = "(.-)" .. pat
-   local last_end = 1
-   local s, e, cap = str:find(fpat, 1)
-   while s do
-      if s ~= 1 or cap ~= "" then
-	   table.insert(t,cap)
-      end
-      last_end = e+1
-      s, e, cap = str:find(fpat, last_end)
-   end
-   if last_end <= #str then
-      cap = str:sub(last_end)
-      table.insert(t, cap)
-   end
-   return t
+    local t = {}
+    local fpat = "(.-)" .. pat
+    local last_end = 1
+    local s, e, cap = str:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(t, cap)
+        end
+        last_end = e + 1
+        s, e, cap = str:find(fpat, last_end)
+    end
+    if last_end <= #str then
+        cap = str:sub(last_end)
+        table.insert(t, cap)
+    end
+    return t
 end
 
+-- Get general battleground info
+function GetBGInfo()
+    -- Counters
+    local htanks = 0;
+    local hheals = 0;
+    local hmelee = 0;
+    local hranged = 0;
+    local hplayers = 0;
+    local hnames = "";
+    local atanks = 0;
+    local aheals = 0;
+    local amelee = 0;
+    local aranged = 0;
+    local aplayers = 0;
+    local anames = "";
+    local specName = "";
 
--- get general battleground info
-function GetBGInfo ()
-    -- counters
-    local htanks=0;
-    local hheals=0;
-    local hmelee=0;
-    local hranged=0;
-    local hplayers=0;
-    local hnames="";
-    local atanks=0;
-    local aheals=0;
-    local amelee=0;
-    local aranged=0;
-    local anames="";
-    local aplayers=0;
-    
-    --loop through leaderboard, grab specs 
-    for i=1, GetNumBattlefieldScores() do
-        name, killingBlows, honorableKills, deaths, honorGained, 
-        faction, race, class, classToken, damageDone, healingDone, 
-        bgRating, ratingChange, preMatchMMR, mmrChange, talentSpec = GetBattlefieldScore(i);
-        local role=GetRoleFromSpec(talentSpec);
-        local unitname = split(name, "-");
-        if faction==1 then --alliance
-            aplayers=aplayers+1;
-            if role == "Healer" then 
-                aheals=aheals+1;
-               anames=anames.."["..unitname[1].."] ";
-            elseif role == "Tank" then 
-                atanks=atanks+1;
-            elseif role == "Melee" then 
-                amelee=amelee+1;
-            elseif role == "Ranged" then 
-                aranged=aranged+1
-            end
-        else --horde
-            hplayers=hplayers+1;
-            if role == "Healer" then 
-                hheals=hheals+1;
-                hnames=hnames.."["..unitname[1].."] ";
-            elseif role == "Tank" then 
-                htanks=htanks+1;
-            elseif role == "Melee" then 
-                hmelee=hmelee+1;
-            elseif role == "Ranged" then 
-                hranged=hranged+1
+    -- Horde
+    for i = 1, 40 do
+        name, realm, class, role, spec = GetBattlefieldScore(i);
+        faction = GetBattlefieldTeamInfo(0);
+        if name then
+            faction = GetBattlefieldTeamInfo(0);
+            if faction == "Horde" then
+                hplayers = hplayers + 1;
+                if (role == "TANK") then htanks = htanks + 1; end
+                if (role == "HEALER") then hheals = hheals + 1; end
+                if (role == "DAMAGER") then
+                    if (spec == "Melee") then hmelee = hmelee + 1; end
+                    if (spec == "Ranged") then hranged = hranged + 1; end
+                end
+                hnames = hnames .. name .. ",";
             end
         end
     end
-    
-    local leader = GetRaidLeader();
-   
-    -- write report
-    local txt=aplayers.." Alli:"..atanks.." Tanks, "..amelee.." Melee, "..aranged.." Rng";
-    SendChatMessage(txt, output);
-    if aheals>0 then
-        txt=aheals.." Heals:"..anames;
-        SendChatMessage(txt, output);
-    end
-    txt=hplayers.." Horde:"..htanks.." Tanks, "..hmelee.." Melee, "..hranged.." Rng";
- 
-    SendChatMessage(txt, output);
-    if hheals>0 then
-        txt=hheals.." Heals:"..hnames;
-        SendChatMessage(txt, output);
-    end
-    SendChatMessage("Leader:"..leader, output);
-end
 
-function GetHealers ()
-    -- counters
-    local hheals=0;
-    local hnames="";
-    local aheals=0;
-    local anames="";
-
-    
-    --loop through leaderboard, grab specs 
-    for i=1, GetNumBattlefieldScores() do
-        name, killingBlows, honorableKills, deaths, honorGained, 
-        faction, race, class, classToken, damageDone, healingDone, 
-        bgRating, ratingChange, preMatchMMR, mmrChange, talentSpec = GetBattlefieldScore(i);
-        local role=GetRoleFromSpec(talentSpec);
-        local unitname = split(name, "-");
-        if faction==1 then --alliance
-            if role == "Healer" then 
-                aheals=aheals+1;
-               anames=anames.."["..unitname[1].."] ";
-            end
-        else --horde
-            if role == "Healer" then 
-                hheals=hheals+1;
-                hnames=hnames.."["..unitname[1].."] ";
+    -- Alliance
+    for i = 1, 40 do
+        name, realm, class, role, spec = GetBattlefieldScore(i);
+        faction = GetBattlefieldTeamInfo(1);
+        if name then
+            faction = GetBattlefieldTeamInfo(1);
+            if faction == "Alliance" then
+                aplayers = aplayers + 1;
+                if (role == "TANK") then atanks = atanks + 1; end
+                if (role == "HEALER") then aheals = aheals + 1; end
+                if (role == "DAMAGER") then
+                    if (spec == "Melee") then amelee = amelee + 1; end
+                    if (spec == "Ranged") then aranged = aranged + 1; end
+                end
+                anames = anames .. name .. ",";
             end
         end
     end
- 
 
-    if aheals>0 then
-        txt=aheals.."Alli Healers:"..anames;
-        SendChatMessage(txt, output);
-		else
-		SendChatMessage("Alliance have no healers!", output);
-    end
-    if hheals>0 then
-        txt=hheals.."Horde Heals:"..hnames;
-        SendChatMessage(txt, output);
-		else
-		SendChatMessage("Horde have no healers!", output);
-    end
-end
-
--- display ashran info text
-function GetAshranInfo ()
-    
-    -- check if ashran, do special check for ashran
-    
-    np=GetNumGroupMembers(); 
-    healers=0;
-    healerNames="";
-    tanks=0;
-    local leader = GetRaidLeader();
-    txt = txt..leader.." is leader\n"..healers.." heals found\n";   
+    local txt = "Info BG: H " .. hplayers .. " (t:" .. htanks .. " h:" .. hheals .. " m:" .. hmelee .. " r:" .. hranged .. ")\n";
+    txt = txt .. "A " .. aplayers .. " (t:" .. atanks .. " h:" .. aheals .. " m:" .. amelee .. " r:" .. aranged .. ")\n";
     INFO_TextMessage(txt);
 end
 
-function GetSpecInfo ()
-    local currentSpec = GetSpecialization()
-    local currentSpecName = currentSpec and select(2, GetSpecializationInfo(currentSpec)) or "None"
-    print("Your current spec:", currentSpecName)
+function GetHealers()
+    local healers = "";
+    for i = 1, 40 do
+        name, realm, class, role, spec = GetBattlefieldScore(i);
+        if name then
+            if role == "HEALER" then
+                healers = healers .. name .. ",";
+            end
+        end
+    end
+    local txt = "Healers: " .. healers;
+    INFO_TextMessage(txt);
 end
 
-function GetSpecStatPriority (currentSpecName)
-    
+-- Get spec and role info
+function GetSpecInfo()
+    local num = 0;
+    for i = 1, GetNumGroupMembers() do
+        local name, _, _, _, _, _, _, _, _, role, _ = GetRaidRosterInfo(i);
+        if name then
+            local spec = GetSpecializationInfo(GetSpecialization());
+            role = GetRoleFromSpec(spec);
+            if role == "Tank" then
+                num = num + 1;
+            end
+        end
+    end
+    local txt = "Info Spec: "..num.." tanks";
+    INFO_TextMessage(txt);
 end
-
-
--- add delete text into confirm delete box
-hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"],"OnShow",function(s) s.editBox:SetText(DELETE_ITEM_CONFIRM_STRING) end)
--- get rid of role call
-RolePollPopup:UnregisterEvent("ROLE_POLL_BEGIN");
-
---reposition tooltip
-GameTooltip:SetScript("OnTooltipSetUnit", function(self)
-    self:ClearAllPoints()
-    self:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", -50, 230)
-end)
---
+++ README.md
@@ -0,0 +1,46 @@
+# WoW Battleground Helper
+
+This is a World of Warcraft addon designed to provide helpful information during battlegrounds. It lists the opposing team's breakdown, highlighting healer names, and provides other useful battleground text functions.
+
+## Usage
+
+To use the addon, load it into your World of Warcraft client. You can then use the following slash commands to access the various functions provided by the addon:
+
+### Slash Commands
+
+- `/info me` - Displays server, title, and stats information.
+- `/info bg` - Provides a breakdown of the battleground teams, including the number of tanks, healers, melee, and ranged players on each side.
+- `/info stats` - Shows specialization and role information for your raid or party.
+- `/info sticky` - Toggles sticky mode for on-screen text messages.
+- `/info healers` - Lists the names of all healers in the battleground.
+- `/info cleantracker` - Cleans up the objective and achievement tracker.
+- `/info cleanbags` - Removes items of rarity below rare (green) from your bags.
+- `/info emptybags` - Removes all items from your bags, except Hearthstone.
+- `/info vendorall` - Sells all items of rarity below rare (green) to a vendor.
+- `/info` - Displays the list of available commands.
+
+## Features
+
+- **Team Breakdown**: Get a quick overview of the number of tanks, healers, melee, and ranged players on both the Horde and Alliance sides.
+- **Healer Highlighting**: Quickly identify all healers in the battleground.
+- **Sticky Text Messages**: Keep important messages on your screen for a longer duration.
+- **Bag Management**: Clean up your bags by removing or selling low-rarity items.
+- **Objective Tracker Cleanup**: Easily reset your objective and achievement tracker.
+
+## Installation
+
+1. Download the addon files.
+2. Extract the files to your World of Warcraft `Interface/AddOns` directory.
+3. Restart World of Warcraft and ensure the addon is enabled in the AddOns menu.
+
+## Contributing
+
+If you'd like to contribute to the development of this addon, please feel free to fork the repository and submit pull requests. Your contributions are welcome!
+
+## License
+
+This addon is licensed under the MIT License. See the `LICENSE` file for more details.
+
+---
+
+Happy gaming!
